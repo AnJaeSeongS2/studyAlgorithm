3c3e46c83e901ddb50f7af18463079904d78a847
Jaeseong An <jindam91@naver.com>
2020-09-05 21:22:32 +0900

    [programmers][level2] clear?

---
+++ src/test/java/com/sonan/programmers/skillChecks/level2/Solution7Test.java
@@ -0,0 +1,16 @@
+package com.sonan.programmers.skillChecks.level2;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+/**
+ * Created by Jaeseong on 2020/08/22
+ * Git Hub : https://github.com/AnJaeSeongS2
+ */
+class Solution7Test {
+
+    @Test
+    void solution() {
+        assertEquals(new int[]{2,1}, new com.sonan.programmers.skillChecks.level2.Solution7().solution(new int[]{93, 30, 55}, new int[]{1,30,5}));
+    }
+}
